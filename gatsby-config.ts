eb647cdb7d36734a47de499dc7c033ddc7930cf6
cjanosdev <CjanosDev@protonmail.com>
2024-09-10 21:20:46 -0400

    cleanup

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,7 +1,7 @@
 import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
-  // pathPrefix: "/catrina.janos",
+  pathPrefix: "/catrina.janos",
   siteMetadata: {
     title: `Catrina Janos`,
     siteUrl: `https://www.yourdomain.tld`
